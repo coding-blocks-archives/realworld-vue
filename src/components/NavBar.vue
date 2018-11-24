3b73df719bccaeb8164a59d269f760b728da469b
Arnav Gupta <arnav@codingblocks.com>
2018-11-24 22:09:03 +0530

    add view templates and css from realworld.io
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">conduit</a>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <a class="nav-link active" href="">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">
+            <i class="ion-compose"></i>&nbsp;New Post
+          </a>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/settings">
+            <i class="ion-gear-a"></i>&nbsp;Settings
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/login"> Sign in </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register"> Sign up </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
